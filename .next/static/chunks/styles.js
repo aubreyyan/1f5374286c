8462674a6edbbad551c850c3d9258ec123bd5e2f
aubrey-y <aubreym.yan@gmail.com>
2019-06-21 19:42:24 -0500

    modified sanitized.js to reeplace &amp; with ampersands

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1561162930926");
+            injectCss(link, link.href.split("?")[0] + "?unix=1561163982748");
           });
       }
     }
